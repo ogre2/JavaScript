055bb9fc80ced69406ab5eecd745235fde3658bc
Stephen <hellostephenwm@gmail.com>
2021-07-22 21:03:25 -0400

    First two chapters.

---
+++ HelloWorld/alert.js
@@ -0,0 +1,2 @@
+// Using the JS alert function
+alert('I\'m JavaScript!');
---
+++ Variables/variables.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let message = "Hello World";
+
+    /**
+     * @description Using the JS alert function
+     * @param message variable
+     */
+    alert(message);
+
+    let admin, name;
+
+    admin = name = 'John';
+
+    alert(admin);
+
+    let planetEarth = 'Earth';
+    let currentUser = 'User';
+});
